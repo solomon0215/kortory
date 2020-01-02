72b3a0ad27d5bae620508884a0e97732b8bb18e3
USER <USER@DESKTOP-U6UALTU>
2020-01-02 23:20:12 +0900

    인사부 푸시

--- geumgang/src/main/java/Controller/User/UserRecruitController.java
+++ geumgang/src/main/java/Controller/User/UserRecruitController.java
@@ -1,5 +1,24 @@
 package Controller.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import Command.Insa.RecruitRegiCommand;
+import Service.User.UserRecruitService;
+
+@Controller
 public class UserRecruitController {
+	@Autowired
+	UserRecruitService userRecruitService;
+	
+	@RequestMapping(value="/user/recruitList", method=RequestMethod.GET)
+	public String recruitList(RecruitRegiCommand rc, Model model) {
+		userRecruitService.recruitSelect(model);
+		model.addAttribute("userPage","../user/recruitList.jsp");
+		return "User/userMain";
+	}
 
 }
